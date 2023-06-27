ef30fdb683e4b7ea8790526e98b2e4606cc46e3e
Leon-Chen1999 <rh22055@bristol.ac.uk>
2023-06-27 22:11:05 +0100

    modify the unit

--- codeTemplate/heatFrontEnd/src/pages/Home.js
+++ codeTemplate/heatFrontEnd/src/pages/Home.js
@@ -20,6 +20,7 @@ const Home = () => {
   ];
 
   const [events, setEvents] = useState([]);
+  const [unitNameCounts, setUnitNameCounts] = useState({});
 
   useEffect(() => {
     loadCalendarEvents();
@@ -28,6 +29,14 @@ const Home = () => {
   const loadCalendarEvents = async () => {
     const result = await axios.get("http://localhost:8080/calendarEvents");
     setEvents(result.data);
+
+    //Caculate the number of each unit name
+    const counts = {};
+    result.data.forEach((event) => {
+      const { unitName } = event;
+      counts[unitName] = counts[unitName] ? counts[unitName] + 1 : 1;
+    });
+    setUnitNameCounts(counts);
   };
 
   const handleEventClick = (event) => {
@@ -39,21 +48,29 @@ const Home = () => {
     const endDate = new Date(event.end);
 
     // Fixed timeline start date
-    const timelineStart = new Date('2022-09-01');
+    const timelineStart = new Date("2022-09-01");
 
     // Calculate the number of days between the event start and end
-    const durationDays = (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24) + 1;
+    const durationDays =
+      (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24) + 1;
 
     // Calculate the number of days between the start of the timeline and the start of the event
-    const offsetDays = (startDate.getTime() - timelineStart.getTime()) / (1000 * 60 * 60 * 24);
+    const offsetDays =
+      (startDate.getTime() - timelineStart.getTime()) / (1000 * 60 * 60 * 24);
 
     return {
       left: `calc(${(offsetDays / 365) * 100}% + 10px)`,
       width: `calc(${(durationDays / 365) * 100}% - 20px)`,
-      backgroundColor: event.type === 'SUMMATIVE' ? 'red' : event.type ==='FORMATIVE' ? 'green' : event.type ==='CapstoneSummative'? 'purple': 'default-color',
+      backgroundColor:
+        event.type === "SUMMATIVE"
+          ? "red"
+          : event.type === "FORMATIVE"
+          ? "green"
+          : event.type === "CapstoneSummative"
+          ? "purple"
+          : "default-color",
     };
   };
-
 
   return (
     <div className="timeline-container">
@@ -61,9 +78,15 @@ const Home = () => {
       <div className="header-container">
         <h1 className="title">Academic Calendar</h1>
         <div className="nav-buttons">
-          <Link to="/" className="nav-button">By Year</Link>
-          <Link to="/weeklyCalendar" className="nav-button">By Term</Link>
-          <Link to="/calendarByModule" className="nav-button">By Module</Link>
+          <Link to="/" className="nav-button">
+            By Year
+          </Link>
+          <Link to="/weeklyCalendar" className="nav-button">
+            By Term
+          </Link>
+          <Link to="/calendarByModule" className="nav-button">
+            By Module
+          </Link>
         </div>
       </div>
 
@@ -79,29 +102,44 @@ const Home = () => {
       {/* Events */}
       <div className="events-container">
         <div className="unitNames-container">
-          {events.map((event, index) => (
-            <div
-              className="unitName"
-              key={index}
-              style={{ top: `${index * 30}px` }}
-            >
-              {event.unitName}
-            </div>
-          ))}
+          {events.map((event, index) => {
+            const isSameUnit =
+              index > 0 && events[index - 1].unitName === event.unitName;
+            const unitName = isSameUnit ? "" : event.unitName;
+            const occurrenceCount = unitNameCounts[event.unitName] || 0;
+            const unitHeight = occurrenceCount * 30;
+
+            return (
+              <React.Fragment key={index}>
+                {isSameUnit && (
+                  <div className="unitName-placeholder" style={{ height: "30px" }}></div>
+                )}
+                {!isSameUnit && (
+                  <div
+                    className="unitName"
+                    style={{ height: `${unitHeight}px`, top: `${index * 30}px`,}}
+                  >
+                    {unitName}
+                  </div>
+                )}
+              </React.Fragment>
+            );
+          })}
         </div>
         {events.map((event, index) => (
           <div
             className="event"
             key={index}
-            style={{ ...getEventStyle(event), top: `${index * 30}px` }}
+            style={{
+              ...getEventStyle(event),
+              top: `${index * 30}px`,
+            }}
             onClick={() => handleEventClick(event)}
           >
             {event.title}
           </div>
         ))}
       </div>
-
-
     </div>
   );
 };
