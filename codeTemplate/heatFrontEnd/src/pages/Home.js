664e1e7164f200131361e54c1dd5cbee015b84de
Yu Tian <fg22142@bristol.ac.uk>
2023-06-27 20:15:46 +0100

    my commit message

--- codeTemplate/heatFrontEnd/src/pages/Home.js
+++ codeTemplate/heatFrontEnd/src/pages/Home.js
@@ -37,23 +37,23 @@ const Home = () => {
   const getEventStyle = (event) => {
     const startDate = new Date(event.start);
     const endDate = new Date(event.end);
-    
+
     // Fixed timeline start date
     const timelineStart = new Date('2022-09-01');
-  
+
     // Calculate the number of days between the event start and end
     const durationDays = (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24) + 1;
-  
+
     // Calculate the number of days between the start of the timeline and the start of the event
     const offsetDays = (startDate.getTime() - timelineStart.getTime()) / (1000 * 60 * 60 * 24);
-  
+
     return {
       left: `calc(${(offsetDays / 365) * 100}% + 10px)`,
       width: `calc(${(durationDays / 365) * 100}% - 20px)`,
       backgroundColor: event.type === "SUMMATIVE" ? "red" : "green",
     };
   };
-  
+
 
   return (
     <div className="timeline-container">
@@ -66,7 +66,7 @@ const Home = () => {
           <Link to="/calendarByModule" className="nav-button">By Module</Link>
         </div>
       </div>
-      
+
       {/* Months */}
       <div className="months-container">
         {months.map((month) => (
@@ -78,6 +78,17 @@ const Home = () => {
 
       {/* Events */}
       <div className="events-container">
+        <div className="unitNames-container">
+          {events.map((event, index) => (
+            <div
+              className="unitName"
+              key={index}
+              style={{ top: `${index * 30}px` }}
+            >
+              {event.unitName}
+            </div>
+          ))}
+        </div>
         {events.map((event, index) => (
           <div
             className="event"
@@ -89,6 +100,8 @@ const Home = () => {
           </div>
         ))}
       </div>
+
+
     </div>
   );
 };
