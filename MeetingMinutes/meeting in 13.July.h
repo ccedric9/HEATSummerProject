9bedddf25a463b0bf25e1240158bb16de982135a
DylaQin <dyla1024@outlook.com>
2023-07-13 14:27:01 +0100

    meeting notes

---
+++ MeetingMinutes/meeting in 13.July.h
@@ -0,0 +1,35 @@
+//
+//  meeting in 13.July.h
+//  meeting
+//
+//  Created by Dyla Q on 2023/7/13.
+//
+
+#ifndef meeting_in_13_July_h
+#define meeting_in_13_July_h
+
+
+#endif /* meeting_in_13_July_h */
+
+1. Discuss how to set the login page:
+    - [ ] we decide to offer login page with own username and password, store by database, offer superpower for staffs
+    - [ ] After login in, we decide to add extra home page for staff, offering staffs extra authority
+    - [ ] The staff no need to sign in, we offer superpower account for them.
+2. Add log in/out, sign in/out page, extra page for staff : Tianyu
+3. Update database: Tianyu
+4. Add footer, update the theme: Cedric
+5. update the dependency line: Dyla
+6. Add hover to the module page, change the timeline: Zhiyuan
+7. Update the report: Haoyan
+8. Assessment height and line adjustment: Haoyan
+9. Sent Joel our current website: next Tuesday
+10. Postpone telling Joel the possibility we have to switch the independent line to gantt chart
+
+Particpant:
+    Dyla Qin
+    Yu Tian
+    Leon Chen
+    Cedric Chen (online)
+    Haoyan Wang (online)
+
+Time: July 13, 2023
---
+++ MeetingMinutes/meeting in July 10.h
@@ -0,0 +1,32 @@
+//
+//  meeting in July 10.h
+//  meeting
+//
+//  Created by Dyla Q on 2023/7/13.
+//
+
+#ifndef meeting_in_July_10_h
+#define meeting_in_July_10_h
+
+
+#endif /* meeting_in_July_10_h */
+
+
+1. All the module should be updated in Wednesday , send to Joel on Thursday
+2. The module part:
+3. Add credit point
+4. The name and code should be in the left bottom
+5. The color of the font should be white
+6. The assessment proportion should be adjusted
+7. later: link the event to the page of specific info after Leon develop the page
+8. Tianyu: update the database
+9. Cedirc: try adding footer section
+
+Paticipants: (online)
+    Dyla Qin
+    Cedric Chen
+    Haoyan Wang
+    Yu Tian
+    Leon Chen
+
+Time: July 10, 2023
