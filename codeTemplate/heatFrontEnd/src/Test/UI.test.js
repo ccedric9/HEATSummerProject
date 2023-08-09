2b77c20baa4914bec2f0146e91f6d46429d51e10
Cedric Chen <tsumong@gmail.com>
2023-08-09 20:06:29 +0100

    update test script with user login state

--- codeTemplate/heatFrontEnd/src/Test/UI.test.js
+++ codeTemplate/heatFrontEnd/src/Test/UI.test.js
@@ -1,63 +1,21 @@
 import React from 'react'
 import { Screen,render,cleanup} from '@testing-library/react'
 import Navbar from '../layout/Navbar'
+import Home from "../pages/Home"
 import Login from '../services/Login'
 import { Provider } from 'react-redux'
 import store from '../redux/store'
 import { MemoryRouter } from 'react-router-dom';
-// const { setupDatabase, cleanupDatabase } = require('jest-mysql');
+import userEvent from '@testing-library/user-event';
+import { MockAuthProvider } from './MockUser'
+
 
 afterEach(()=>{
     cleanup();
 })
 
-// setupDatabase();
-
-
-// jest.mock('react-router-dom', () => ({
-//     ...jest.requireActual('react-router-dom'),
-//     useNavigate: jest.fn(),
-// }));
-
-// module.exports = async () => {
-//     await cleanupDatabase();
-// };
-
-// const mysql = require('mysql2/promise');
-// const { query } = require('jdbc:mysql://amazonheatdb.cczulapjuexr.eu-west-2.rds.amazonaws.com:3306/heatDB');
-
-// describe('Database Tests', () => {
-//   it('should fetch data from the database', async () => {
-//     // Perform your test setup here if needed
-//     // ...
-
-//     // Perform a database query using your database module
-//     const results = await query('SELECT * FROM calendar_event');
-
-//     // Perform assertions based on the fetched data
-//     expect(results.length).toBeGreaterThan(0);
-
-//     // Perform your test cleanup here if needed
-//     // ...
-//   });
-// });
-
-// test('check', ()=>{
-//     expect('1').toEqual('1');
-// })
-
-// test('Check navigation bar display information',()=>{
-//     const {getAllByRole} = render( 
-//         <MemoryRouter>
-//         <Navbar />
-//       </MemoryRouter>
-//     );
-//     const iconButtons = getAllByRole('IconButton');
-//     console.log(iconButtons);
-//     expect(iconButtons).toBe(4);
-// })
-
 test('Check login page',()=>{
+
     const {getAllByRole} = render(
         <Provider store = {store}>
             <MemoryRouter>
@@ -66,5 +24,23 @@ test('Check login page',()=>{
         </Provider>
      );
     const button = getAllByRole('button');
-    expect(button.length).toBe(1);
+    expect(button.length).toEqual(1);
 })
+
+test('should render MyComponent with a logged-in user', () => {
+    // Set up the logged-in user state in the mock auth provider
+    const { getByText } = render(
+        <Provider store = {store}>
+            <MemoryRouter>
+                <MockAuthProvider >
+                    <Home/>
+                </MockAuthProvider>
+            </MemoryRouter>
+        </Provider>
+
+    );
+  
+    // Now you can test your component's behavior with the simulated logged-in user
+    // For example, assert that the username is displayed
+    expect(getByText('Computer Science')).toBeInTheDocument();
+  });
