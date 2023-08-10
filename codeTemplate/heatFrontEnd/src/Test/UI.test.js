df11825c74506f6c8efb3707026ded001a6ef2ab
Cedric Chen <tsumong@gmail.com>
2023-08-10 20:48:01 +0100

    login as staff test

--- codeTemplate/heatFrontEnd/src/Test/UI.test.js
+++ codeTemplate/heatFrontEnd/src/Test/UI.test.js
@@ -1,6 +1,8 @@
 const {Builder,By,Key} = require('selenium-webdriver');
+const assert = require('assert');
 
-async function textUI(){
+    const homepageURL = "http://localhost:3000/home";
+async function loginAsStaff(){
     let driver = await new Builder().forBrowser('chrome').build();
     try{
         await driver.get("http://localhost:3000/login");
@@ -12,12 +14,20 @@ async function textUI(){
         await password.sendKeys("password");
 
         const submit = await driver.findElement(By.id("submit-btn"));
-
         await submit.click();
-        await driver.sleep(9000);
+        await driver.sleep(1000);
+        //check current web page redirect to Home page after login sucess
+        const currentURL = (await driver.getCurrentUrl()).toString();
+        assert.strictEqual(currentURL,homepageURL,"Test Failed: " + "URL doesn't direct to homepage after login");
+
+        //check current web page contains correct elements
+        // if contains program name 
+        // if display corrent year
+        // if display corrent assessment information
+        
     }   finally{
         // await driver.quit();
     }
-} 
+}
 
-textUI();
+loginAsStaff();
